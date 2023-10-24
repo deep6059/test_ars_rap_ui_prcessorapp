18a455fe476bc81143f51338c33042da1d2dd3d0
Arkaprabha Rudra Sarma <arkaprabha.rudrasarma@mt.com>
2023-10-24 08:19:52 +0000

    Arka's first stable version of the RAP app

---
+++ mttravel/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'mttravel/test/integration/FirstJourney',
+		'mttravel/test/integration/pages/TravelList',
+		'mttravel/test/integration/pages/TravelObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, TravelList, TravelObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('mttravel') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheTravelList: TravelList,
+					onTheTravelObjectPage: TravelObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
